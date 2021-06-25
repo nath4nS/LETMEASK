0e5a85bc8a1942a20b10c279155e2f22c5f75d3b
nath4ns <nathansousa360@gmail.com>
2021-06-25 16:43:50 -0300

    Segunda aula

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,23 @@
 import { useHistory } from 'react-router-dom';
 
-import { auth, firebase } from '../services/firebase';
-
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
 import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss';
 
 export function Home() {
     const history = useHistory();
+    const { user, signInWithGoogle } = useAuth()
 
-    function handleCreateRoom() {
+    async function handleCreateRoom() {
+        if (!user) {
+            await signInWithGoogle()
+        }
+
             history.push('/rooms/new');
         }
 
